7441b13a49aa6b653dd89a3da292da559dbfe9f0
Aishwary200 <amritakeshri3059@gmail.com>
2021-03-27 16:34:16 +0530

    added avatar

--- component/customSideBarMenu.js
+++ component/customSideBarMenu.js
@@ -1,12 +1,76 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+import { Avatar } from 'react-native-elements'
 import { DrawerItems } from 'react-navigation-drawer'
 import firebase from 'firebase';
 
 export default class CustomSideBarMenu extends Component {
+    constructor() {
+        super()
+        this.state = {
+            userId: firebase.auth().currentUser.email,
+            name: '',
+            image: '#',
+            docId: ''
+        }
+    }
+    fetchImage = (imageName) => {
+        var storageRef = firebase.storage().ref().child("user_profiles" + imageName)
+        storageRef.getDownloadURL().then((uri) => {
+            this.setState({
+                image: uri
+            })
+        })
+            .catch((error) => {
+                this.setState({
+                    image: '#'
+                })
+            })
+    }
+    getUserProfile = () => {
+        db.collection('users').where('email_id', '==', this.state.userId)
+            .onSnapshot((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    this.setState({
+                        name: doc.data().first_name + ' ' + doc.data().last_name
+                    })
+                })
+            })
+    }
+    uploadImage = async (uri, imageName) => {
+        var response = await fetch(uri);
+        var blob = await response.blob();
+        var ref = firebase.storage().ref().child('user_profiles/' + imageName);
+        return ref.put(blob).then((response) => {
+            this.fetchImage(imageName)
+        })
+    }
+    selectPicture = async () => {
+        const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1
+        })
+        if (!cancelled) {
+            this.uploadImage(uri, this.state.userId)
+        }
+    }
+    componentDidMount() {
+        this.fetchImage(this.state.userId)
+        this.getUserProfile()
+    }
     render() {
         return (
             <View style={{ flex: 1 }}>
+                <View>
+                    <Avatar rounded source={{ uri: this.state.image }} size='medium' onPress={() => {
+                        this.selectPicture();
+                    }} containerStyle={styles.imageContainer} showEditButton
+                    />
+                    <Text style={{ fontWeight: '100', fontSize: 20, paddingTop: 10 }}>{this.state.name}</Text>
+                </View>
                 <View style={styles.drawerItemsContainer}>
                     <DrawerItems {...this.props} />
                 </View>
